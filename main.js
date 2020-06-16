cdec75fe6b98d386b30964d2bac7b727dae9e3e4
thatzenguy <00yoursenpai00@gmail.com>
2020-06-16 22:36:22 +0300

    ayaaa

--- main.js
+++ main.js
@@ -12,7 +12,11 @@ var savegame = JSON.parse(localStorage.getItem("pointSave"))
     if (savegame !== null) {
       gameData = savegame
     }
-    
+
+document.getElementById("pointsGotten").innerHTML = gameData.points + " Points"
+document.getElementById("pointsPerClick").innerHTML = gameData.pointsPerClick + " Points Per Click"
+document.getElementById("perClickUpgrade").innerHTML = "Upgrade Points Per Click (COST:" + gameData.pointsPerClickCost + ")"
+
 function addPoints() {
     gameData.points += gameData.pointsPerClick
     document.getElementById("pointsGotten").innerHTML = gameData.points + " Points"
@@ -25,6 +29,6 @@ function buyUpgrade() {
         gameData.pointsPerClickCost *= 1.2
         document.getElementById("pointsGotten").innerHTML = gameData.points + " Points"
         document.getElementById("pointsPerClick").innerHTML = gameData.pointsPerClick + " Points Per Click"
-        document.getElementById("perClickUpgrade").innerHTML = "Upgrade Points Per Click"
+        document.getElementById("perClickUpgrade").innerHTML = "Upgrade Points Per Click (COST:" + gameData.pointsPerClickCost + ")"
     }
 }
